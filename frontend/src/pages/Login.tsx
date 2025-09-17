183f7e7fbbc527939dbf032707af35746876b78d
tariq3101 <khantariq10648@gmail.com>
2025-09-17 20:38:48 +0530

    for deployment

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
-import API from "@/api/axios";  // ⬅️ Axios instance
+import API from "../api/axios.js"; // ⬅️ Axios instance
 
 const Login = () => {
   const [formData, setFormData] = useState({
