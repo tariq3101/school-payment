0d292239adc4e5aa93ad3e27f173a939d8945cc5
tariq3101 <khantariq10648@gmail.com>
2025-09-17 21:20:11 +0530

    for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api", // adjust to your backend URL
+  baseURL: process.env.REACT_APP_API_URL
+    ? process.env.REACT_APP_API_URL + "api"
+    : "http://localhost:5000/api",
 });
 
 export default API;
